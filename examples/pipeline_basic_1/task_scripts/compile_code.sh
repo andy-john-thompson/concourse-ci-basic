6399d20c418197084c76ea5ac0b6e52866178f63
Thompson Andrew <athompson@devoteam.com>
2020-05-18 15:20:01 +0100

    updates

--- examples/pipeline_basic_1/task_scripts/compile_code.sh
+++ examples/pipeline_basic_1/task_scripts/compile_code.sh
@@ -8,7 +8,7 @@ echo "..nothing here"
 
 echo "compiling the code..."
 #this stub represents the compilation step (eg some npm/maven compilation etc that produces build artifacts)
-echo "this is the product" > "my-build-artifacts/product.txt"
+echo "this is the compiled artifact" > "my-build-artifacts/product.txt"
 
 echo "check the contents of the my-build-artifacts folder again"
 ls "my-build-artifacts"
