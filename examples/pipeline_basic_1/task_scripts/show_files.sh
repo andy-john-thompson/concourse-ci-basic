d600c2c7aaf8f8a0e77e8e70816fb875f42b40fa
Thompson Andrew <athompson@devoteam.com>
2020-05-18 15:13:24 +0100

    add 2nd stage

---
+++ examples/pipeline_basic_1/task_scripts/show_files.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "stub to represent some secondary step.."
+echo "stub shows the contents of the built artifact, passed in from task 1:"
+ls my-build-artifacts/*
+cat "my-build-artifacts/product.txt"
